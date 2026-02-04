21411c5ea2c1aff5eb30018bec42fdc0952083bd
sytnic <dm.sytnic@yandex.ru>
2026-02-04 11:38:12 +0300

    32. CSS processing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+// модуль Node laravel-mix
 const mix = require('laravel-mix');
 
 /*
@@ -13,3 +14,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// преобразование файлов less, если они есть, в готовые css
+// mix.less('path.less', 'public/css/less.css')
+//    .less('path2.less', 'public/css/less2.css');
+
+// создание одного css файла из двух своих css файлов
+// mix.styles(['first.css', 'second.css'], 'public/css/all.css');
